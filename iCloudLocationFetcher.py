d14eee0b7999ac8e28bc7d1f0df4b28cb0245e9b
Erwin de Brouwer <airwinb@users.noreply.github.com>
2017-11-04 20:47:05 +0100

    ILF-4: Use distance based update interval (#6)

--- iCloudLocationFetcher.py
+++ iCloudLocationFetcher.py
@@ -1,5 +1,4 @@
 import abc
-import datetime
 import ConfigParser
 import logging
 import math
@@ -14,9 +13,15 @@ from pyicloud.exceptions import PyiCloudAPIResponseError
 # Configuration
 NR_SECONDS_WHEN_ALWAYS_UPDATE_DOMOTICZ = 3600
 ICLOUD_SESSION_TIMEOUT = 280  # in seconds; 300 seconds is too long
+DEFAULT_RETRIEVE_INTERVAL = ICLOUD_SESSION_TIMEOUT - 10
+OUTDATED_LIMIT = 250  # if icloud location timestamp is older than this, then retry
+OUTDATED_LOCATION_RETRY_INTERVAL = 20
+
+MIN_RETRIEVE_INTERVAL = 10
+MAX_RETRIEVE_INTERVAL = 3600
 
 # Constants (Do not change)
-SCRIPT_VERSION = "0.5.0"
+SCRIPT_VERSION = "0.6.0-SNAPSHOT"
 SCRIPT_DATE = "2017-11-03"
 URL_DISTANCE_PARAM = "__DISTANCE__"
 
@@ -28,15 +33,14 @@ logger = None
 class MonitorDevice(object):
     __metaclass__ = abc.ABCMeta
     logger = None
-    domoticz_server = None
-    domoticz_authorization = None
     monitor_devices = []
 
     def __init__(self, name, update_url):
         self.name = name
         self.update_url = update_url
         self.distance = -1.0
-        self.last_update_time = 0
+        self.location_timestamp = 0
+        self.next_retrieve_timestamp = time.time()
 
     @classmethod
     def set_logger(cls, logger):
@@ -46,18 +50,19 @@ class MonitorDevice(object):
     def set_monitor_devices(cls, devices):
         cls.monitor_devices = devices
 
-    def update(self, distance, last_update_time):
-        send_update = False
-        if distance != self.distance:
+    def update(self, distance, icloud_location_timestamp):
+        previous_distance = self.distance
+        previous_location_timestamp = self.location_timestamp
+        # send update if
+        # 1. real significant change
+        # 2. if we haven't send an update for a long time
+        if (distance != previous_distance and not (abs(distance - previous_distance == 0.1) and (distance >= 5.0))) \
+                or icloud_location_timestamp - previous_location_timestamp > NR_SECONDS_WHEN_ALWAYS_UPDATE_DOMOTICZ:
             self.distance = distance
-            self.last_update_time = last_update_time
-            send_update = True
-        else:
-            if last_update_time - self.last_update_time > NR_SECONDS_WHEN_ALWAYS_UPDATE_DOMOTICZ:
-                self.last_update_time = last_update_time
-                send_update = True
-        if send_update:
+            self.location_timestamp = icloud_location_timestamp
             self.send_to_update_url()
+
+        self.set_next_retrieve_timestamp(previous_distance, previous_location_timestamp)
 
     def send_to_update_url(self):
         url = self.update_url.replace(URL_DISTANCE_PARAM, str(self.distance))
@@ -70,6 +75,21 @@ class MonitorDevice(object):
                 self.logger.debug("%s -> %s" % (url, result))
             except requests.ConnectionError, e:
                 self.logger.error('Request failed %s - %s' % (url, e))
+
+    def get_next_retrieve_timestamp(self):
+        return self.next_retrieve_timestamp
+
+    def set_next_retrieve_timestamp(self, previous_distance, previous_location_timestamp):
+        # retry shortly if the new value is quite old
+        if self.location_timestamp - previous_location_timestamp > OUTDATED_LIMIT:
+            self.next_retrieve_timestamp = time.time() + OUTDATED_LOCATION_RETRY_INTERVAL
+            return
+
+        if self.distance == 0.0 or previous_distance == self.distance:
+            seconds_to_next_retrieve = DEFAULT_RETRIEVE_INTERVAL
+        else:
+            seconds_to_next_retrieve = max(MIN_RETRIEVE_INTERVAL, min(int(30 * self.distance), MAX_RETRIEVE_INTERVAL))
+        self.next_retrieve_timestamp = time.time() + seconds_to_next_retrieve
 
 
 def distance_meters(origin, destination):
@@ -176,7 +196,7 @@ def main():
 
     connected = False
     last_icloud_request_time = 0
-    sleep_time = 270
+    sleep_time = 10
     icloud = None
     while keep_running:
         try:
@@ -188,27 +208,37 @@ def main():
                     sleep_time = 900
                 else:
                     connected = True
+                    sleep_time = 5
                     last_icloud_request_time = time.time()
 
             if connected:
                 for monitor_device in monitor_devices:
-                    apple_device = get_apple_device(icloud.devices, monitor_device.name)
-                    if apple_device is not None:
-                        if apple_device.content['locationEnabled']:
-                            location = apple_device.location()
-                            last_icloud_request_time = time.time()
-                            if location is not None:
-                                logger.debug(location)
-                                location_timestamp = location['timeStamp'] / 1000
-                                device_location = (location['latitude'], location['longitude'])
-                                distance = distance_meters(home_location, device_location)
-                                rounded_distance_km = math.floor(distance / 100) / 10.0
-                                location_date = datetime.datetime.fromtimestamp(location_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-                                logger.info("Device '%s' on %s at %d meter, or rounded at %.1f km" % (monitor_device.name, location_date, distance, rounded_distance_km))
-                                monitor_device.update(rounded_distance_km, location_timestamp)
+                    if monitor_device.get_next_retrieve_timestamp() < now:
+                        logger.debug("Getting update for %s" % monitor_device.name)
+                        apple_device = get_apple_device(icloud.devices, monitor_device.name)
+                        if apple_device is not None:
+                            if apple_device.content['locationEnabled']:
+                                location = apple_device.location()
+                                last_icloud_request_time = time.time()
+                                if location is not None:
+                                    logger.debug(location)
+                                    location_timestamp = location['timeStamp'] / 1000
+                                    device_location = (location['latitude'], location['longitude'])
+                                    distance = distance_meters(home_location, device_location)
+                                    rounded_distance_km = math.floor(distance / 100) / 10.0
+                                    monitor_device.update(rounded_distance_km, location_timestamp)
+                                    now = time.time()
+                                    location_seconds_ago = int(now - location_timestamp)
+                                    next_update = int(monitor_device.get_next_retrieve_timestamp() - now)
+                                    logger.info("Device '%s' was %d seconds ago at %d meter, or rounded at %.1f km. Next update in %d seconds" % (monitor_device.name, location_seconds_ago, distance, rounded_distance_km, next_update))
+                    else:
+                        logger.debug("Skipping update for %s" % monitor_device.name)
 
         except (requests.exceptions.ConnectionError, PyiCloudAPIResponseError) as e:
-            logger.warn("Exception: {0}".format(str(e)))
+            # logger.warn("Exception: {0}".format(str(e)))
+            now = time.time()
+            logger.warn("Now - last_icloud_request_time: %d" % str(now - last_icloud_request_time))
+            logger.exception("Connection error or PyiCloud exception")
             connected = False
 
         time.sleep(sleep_time)
